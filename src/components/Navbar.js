ff7551cc8542f4c6931ea78154bc95eac13d842e
Richard Fu <richardfxr.personal@gmail.com>
2022-07-06 17:22:19 -0700

    🔵RCT: added magic line to Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 // === IMPORTS ============================
 // React
-import { NavLink } from 'react-router-dom';
+import { useEffect, useRef, useState } from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
 // Sass
 import './Navbar.scss';
 // SVGs
@@ -12,17 +13,37 @@ import {ReactComponent as InfoIcon} from '../assets/SVGs/info.svg';
 
 
 export default function Navbar({ setCurrentPage }) {
+    const navUl = useRef(null);
+    const magicLineVrt = useRef(null);
+    const magicLineHrz = useRef(null);
+    const location = useLocation();
+
+    // updates magic line on initial load and on location (route) change
+    useEffect(() => {
+        // find currently active <NavLink />
+        let activeLink = navUl.current.getElementsByClassName('active')[0];
+
+        if (activeLink) {
+            console.log("activeLink.offsetTop:", activeLink.offsetTop);
+            console.log("activeLink.offsetLeft:", activeLink.offsetLeft);
+            // update magic line position if activeLink is not falsy
+            magicLineVrt.current.style.transform = "translateY(" + activeLink.offsetTop + "px)";
+            magicLineHrz.current.style.transform = "translateX(" + activeLink.offsetLeft + "px)";
+        }
+    }, [location])
+
     return (
         <div className='navbar'>
             <header>
                 <Logo className='logo'/>
                 <nav>
-                    <ul>
+                    <ul ref={navUl}>
                         <li><NavigationLink href="/" label="Home." setCurrentPage={setCurrentPage}/></li>
                         <li><NavigationLink href="/projects" label="Projects." setCurrentPage={setCurrentPage}/></li>
                         <li><NavigationLink href="/about" label="About me." setCurrentPage={setCurrentPage}/></li>
                         <li><NavigationLink href="/info" label="Site information." setCurrentPage={setCurrentPage}/></li>
-                        <li className='magicLine' aria-hidden="true"></li>
+                        <li className='magicLine' id="magicLineVrt" ref={magicLineVrt} aria-hidden="true"></li>
+                        <li className='magicLine' id="magicLineHrz" ref={magicLineHrz} aria-hidden="true"></li>
                     </ul>
                 </nav>
             </header>
