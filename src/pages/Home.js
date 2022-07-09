d936654ee32e1aef0f23f826b5d235fb0d322626
Richard Fu <richardfxr.personal@gmail.com>
2022-07-08 22:06:34 -0700

    🔵RCT: added #intro section to Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,34 @@
 // === IMPORTS ============================
+// Sass
+import './Home.scss';
 // components
-import PageTemplate from "../components/PageTemplate"
+import PageTemplate from "../components/PageTemplate";
+import Section from "../components/Section";
+import Button from '../components/Button';
 
 export default function Home() {
     return (
         <PageTemplate>
             <main id="main">
                 <h1>Hello</h1>
+
+                <Section heading="Introduction" id="intro">
+                    <p className="lg">I’m <em>Richard Fu</em>, an <em>artist</em>, <em>designer</em>, and <em>developer</em> who loves blue - <em className="rblue">#11d3e7</em> to be exact.</p>
+                    <ul className='row md'>
+                        <li><Button href='#works' target="_self" styling="sm rblue"><span className='inter upDw pad--right'>↓</span> Works</Button></li>
+                        <li><Button to='/about' styling="sm">About</Button></li>
+                        <li><Button to='/about' styling="sm">Contact</Button></li>
+                    </ul>
+                </Section>
+
+                <Section heading="Selected Works" id="works">
+                    <p className="lg">I’m <em>Richard Fu</em>, an <em>artist</em>, <em>designer</em>, and <em>developer</em> who loves blue - <em className="rblue">#11d3e7</em> to be exact.</p>
+                    <ul className='row md'>
+                        <li><Button href='#works' target="_self" styling="sm">Works</Button></li>
+                        <li><Button to='/about' styling="sm">About</Button></li>
+                        <li><Button to='/about' styling="sm">Contact</Button></li>
+                    </ul>
+                </Section>
             </main>
         </PageTemplate>
     )
