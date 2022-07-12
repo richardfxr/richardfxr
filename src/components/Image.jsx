671991011aa68cf9c02de96c90018ea61359b93c
Richard Fu <richardfxr.personal@gmail.com>
2022-07-12 15:21:46 -0700

    🔵RCT: added motion image

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,8 +1,23 @@
-export default function Image({ folder, fileName, alt}) {
-    return (
-        <picture>
-            <source srcSet={'/images/' + folder + '/' + fileName + '.webp'} />
-            <img src={'/images/' + folder + '/' + fileName + '.jpg'} alt={alt} />
-        </picture>
-    )
+// === IMPORTS ============================
+// Framer Motion
+import { motion } from "framer-motion"
+
+export default function Image({ animate, folder, fileName, alt}) {
+    if (animate === 'layout') {
+        return (
+            <motion.div layoutId={fileName}>
+                <picture>
+                    <source srcSet={'/images/' + folder + '/' + fileName + '.webp'} />
+                    <img src={'/images/' + folder + '/' + fileName + '.jpg'} alt={alt} />
+                </picture>
+            </motion.div>
+        )
+    } else {
+       return (
+            <picture>
+                <source srcSet={'/images/' + folder + '/' + fileName + '.webp'} />
+                <img src={'/images/' + folder + '/' + fileName + '.jpg'} alt={alt} />
+            </picture>
+        )
+    }
 }
