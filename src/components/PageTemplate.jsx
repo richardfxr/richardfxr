95a2229670aab0a31516fa27db86037f8a99158f
Richard Fu <richardfxr.personal@gmail.com>
2022-07-12 15:22:15 -0700

    🔵RCT: added <AnimatePresence> to PageTemplate

--- src/components/PageTemplate.jsx
+++ src/components/PageTemplate.jsx
@@ -1,4 +1,9 @@
 // === IMPORTS ============================
+// React
+import { useEffect } from "react"
+import { useLocation } from "react-router-dom"
+// Framer Motion
+import { motion, AnimatePresence } from "framer-motion"
 // Sass
 import './PageTemplate.scss'
 // components
@@ -6,13 +11,28 @@ import Breadcrumbs from './Breadcrumbs'
 import Footer from '../components/Footer'
 
 export default function PageTemplate({ children }) {
+    const location = useLocation()
+    
+    useEffect(() => {
+        console.log("location changed")
+        // scroll page to top on route change
+        window.scrollTo(0, 0);
+    }, [location])
+
     return (
-        <div className="pageWrapper">
-            <Breadcrumbs />
-            <main id="main">
-                {children}
-            </main>
-            <Footer />
-        </div>
+        <AnimatePresence exitBeforeEnter>
+            <motion.div 
+                className="pageWrapper"
+                initial={{ y: 10, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                exit={{ y: -10, opacity: 0 }}
+                transition={{ duration: 0.2 }}>
+                <Breadcrumbs />
+                <main id="main">
+                    {children}
+                </main>
+                <Footer />
+            </motion.div>
+        </AnimatePresence>
     )
 }
