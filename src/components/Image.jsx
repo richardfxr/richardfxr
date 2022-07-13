b7c30086de769243e2dc1e21a1dcbe0a1f8d89d2
Richard Fu <richardfxr.personal@gmail.com>
2022-07-12 21:35:38 -0700

    🔵RCT: animated <Image>

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -2,10 +2,12 @@
 // Framer Motion
 import { motion } from "framer-motion"
 
-export default function Image({ animate, folder, fileName, alt}) {
-    if (animate === 'layout') {
+export default function Image({ ani, folder, fileName, alt}) {
+    if (ani === 'layout') {
         return (
-            <motion.div layoutId={fileName}>
+            <motion.div 
+                layoutId={fileName}
+                transition={{ ease: [0.5, 0, 0.05, 1], duration: 0.5 }}>
                 <picture>
                     <source srcSet={'/images/' + folder + '/' + fileName + '.webp'} />
                     <img src={'/images/' + folder + '/' + fileName + '.jpg'} alt={alt} />
