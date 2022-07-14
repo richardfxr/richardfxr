c0da87270375bc9875e9857177c513271ecb00ea
Richard Fu <richardfxr.personal@gmail.com>
2022-07-14 14:38:31 -0700

    🔵RCT: added <Table> and styling to Archive page

--- src/pages/Archive.jsx
+++ src/pages/Archive.jsx
@@ -1,10 +1,34 @@
 // === IMPORTS ============================
+// Sass
+import './Archive.scss'
 // components
 import PageTemplate from "../components/PageTemplate"
+import ScrollContainer from "../components/ScrollContainer"
+import Section from "../components/Section"
+import Table from "../components/Table"
+import UnderlinedLink from '../components/UnderlinedLink'
+
+
+
+// === ARCHIVE DATA =======================
+const archive2022 = [
+    ['#', 'title', 'type'],
+    [<UnderlinedLink href="https://www.richardfxr.com/" target="_blank">115° French Press</UnderlinedLink>, 'product design'],
+    ['Exploded 1200D', 'product design'],
+    ['115° French Press', 'product design'],
+    ['Exploded 1200D', 'product design'],
+    ['115° French Press', 'product design'],
+    ['Exploded 1200D', 'product design'],
+]
 
 export default function Archive() {
     return (
-        <PageTemplate heading="Archive">
+        <PageTemplate heading="Archive" id="archive">
+            <Section heading="2022" id="2022" first>
+                <ScrollContainer horizontal>
+                    <Table tableData={archive2022} caption="2022 Archive" indexed />
+                </ScrollContainer>
+            </Section>
         </PageTemplate>
     )
 }
