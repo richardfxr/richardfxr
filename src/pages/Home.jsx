5a977f3e536a06affb29c0e0f28e8fc54b2ed244
Richard Fu <richardfxr.personal@gmail.com>
2022-07-25 16:32:37 -0700

    🔵RCT: added illus to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,14 +5,31 @@ import './Home.scss'
 import { projects } from '../components/AnimatedRoutes'
 import PageTemplate from "../components/PageTemplate"
 import Section from "../components/Section"
+import ScrollContainer from '../components/ScrollContainer'
 import Button from '../components/Button'
 import ProjectCard from '../components/ProjectCard'
+// SVGs
+import {ReactComponent as IllusArt} from '../assets/SVGs/home-art-illus.svg'
+import {ReactComponent as IllusDesign} from '../assets/SVGs/home-design-illus.svg'
+import {ReactComponent as IllusDev} from '../assets/SVGs/home-dev-illus.svg'
 
 export default function Home() {
     return (
         <PageTemplate heading="Hello" id="home">
             <Section heading="Introduction" id="intro" first>
-                <p className="lg inter" aria-label='waving Kaomoji'>(¬_¬)ﾉ</p>
+                <ScrollContainer horizontal>
+                    <ul className='row'>
+                        <li className='illus__container'>
+                            <IllusArt />
+                        </li>
+                        <li className='illus__container'>
+                            <IllusDesign />
+                        </li>
+                        <li className='illus__container'>
+                            <IllusDev />
+                        </li>
+                    </ul>
+                </ScrollContainer>
                 <p className="lg">I’m <em>Richard Fu</em>, an <em>artist</em>, <em>designer</em>, and <em>developer</em> who loves blue - <em className="rblue">#11d3e7</em> to be exact.</p>
                 <ul className='row md'>
                     <li><Button href='#works' target="_self" styling="sm rblue"><span className='inter upDw pad--right'>↓</span> Works</Button></li>
