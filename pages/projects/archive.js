8466d7749862caf795f0e6be2d2b0a94aaca7b56
Richard Fu <richardfxr.personal@gmail.com>
2022-10-14 01:52:40 -0400

    🔵RCT: added Weather Dial to archive

--- pages/projects/archive.js
+++ pages/projects/archive.js
@@ -23,6 +23,7 @@ const sections = [
 // === ARCHIVE DATA =======================
 const archive2022 = [
     ['#', 'title', 'type'],
+    [<UnderlinedLink href="https://weatherdial.richardfxr.com/" target="_blank" key="weatherDial">Weather Dial</UnderlinedLink>, 'web design, dev'],
     [<UnderlinedLink href="https://preview.sity.earth/" target="_blank" key="sity1">sity.earth v1.1</UnderlinedLink>, 'web design, dev, illustration'],
     [<UnderlinedLink href="https://youtu.be/HucnOIo9QKk" target="_blank" key="passersby">Passersby</UnderlinedLink>, 'graphic design, videography'],
     [<UnderlinedLink to="/info" key="richardfxr10">richardfxr.com v10</UnderlinedLink>, 'web design, dev'],
