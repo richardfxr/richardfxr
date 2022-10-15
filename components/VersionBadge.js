1bff5490e16ba802b5be4a39d1f68bfe844c60f7
Richard Fu <richardfxr.personal@gmail.com>
2022-10-15 18:31:41 -0400

    🎉RLS: v10.2.1 gold

--- components/VersionBadge.js
+++ components/VersionBadge.js
@@ -2,7 +2,7 @@ export default function VersionBadge() {
     return (
         <div className="versionBadge" aria-label='version badge'>
             <span className="number">v10.2.1</span>
-            <span className="state">beta</span>
+            <span className="state">gold</span>
         </div>
     )
 }
