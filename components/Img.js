f3d67117399c3879c5fd1dc9f8cb50753d3abb4c
Richard Fu <richardfxr.personal@gmail.com>
2022-12-19 15:28:33 -0500

    🔵RCT: add caption to <Img>

--- components/Img.js
+++ components/Img.js
@@ -15,24 +15,39 @@ const imageVarLowMo = {
     exit: { opacity: 0, transition: { duration: 0.2} },
 }
 
-export default function Img({ img, layoutId, alt, ani, priority }) {
+export default function Img({ img, layoutId, alt, ani, priority, caption, captionAlign, captionBottom }) {
     const isMotionOK = useIsMotionOK()
 
     return (
         <motion.div
             className='img'
+            style={{'--figcaption-bottom': captionBottom + '%'}}
             layoutId={ani === 'layout' ? (isMotionOK ? layoutId : null) : null}
             transition={{ ease: [0.5, 0, 0.05, 1], duration: 0.5 }}
             variants={isMotionOK ? null : imageVarLowMo}
             initial="hidden"
             animate="show"
             exit="exit">
-            <Image
-                src={img}
-                alt={alt}
-                placeholder="blur"
-                priority={priority}
-            />
+            {caption ? 
+                <figure>
+                    <Image
+                        src={img}
+                        alt={alt}
+                        placeholder="blur"
+                        priority={priority}
+                    />
+                    <figcaption className={captionAlign}>
+                        <span>{caption}</span>
+                    </figcaption>
+                </figure>
+                :
+                <Image
+                    src={img}
+                    alt={alt}
+                    placeholder="blur"
+                    priority={priority}
+                />
+            }
         </motion.div>
     )
 }
