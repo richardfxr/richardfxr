5aa842b0d456da8e60d4cfbe8f0c04f8eb068dd9
Richard Fu <richardfxr.personal@gmail.com>
2023-10-11 00:50:45 -0400

    🔵RCT: update resume to version 3

--- pages/about/resume.js
+++ pages/about/resume.js
@@ -6,12 +6,15 @@ import Layout from "../../components/Layout"
 import Section from "../../components/Section"
 import UnderlinedLink from '../../components/UnderlinedLink'
 import Button from '../../components/Button'
+// SVGs
+import Logo from '../../public/SVGs/logo.svg'
 
 
 
 // === SECTIONS ===========================
 const sections = [
     {heading: "Info", id: "info"},
+    {heading: "Experience", id: "experience"},
     {heading: "Education", id: "education"},
     {heading: "Projects", id: "projects"},
     {heading: "Skills", id: "skills"},
@@ -32,8 +35,8 @@ export default function Resume() {
                 <meta property="og:image:height" content="630" />
             </Head>
 
-            <Layout heading="Résumé" id="resume">
-                <Section sections={sections} index={0} skipLinkHidden>
+            <Layout heading="Résumé" id="resume" printHidden>
+                <Section sections={sections} index={0} skipLinkHidden printHidden>
                     <dl className="twoCol dlList">
                         <div>
                             <dt>Legal name</dt>
@@ -53,25 +56,33 @@ export default function Resume() {
                         </div>
                     </dl>
 
-                    <p className='md first'>This is the web version of my résumé, last updated on <em>March 20, 2023</em>. Please use the PDF version below for downloading and printing.</p>
+                    <p className='md first'>This is the web version of my résumé, last updated on <em>October 11, 2023</em>. Please use the PDF version below for downloads and prints.</p>
                     <Button href="/RichardFu-resume.pdf" target="_blank" styling="sm">PDF version</Button>
                 </Section>
+
+                <header id="printHeader" className="screenHidden">
+                    <Logo />
+                    <div className="name">
+                        <h1>Richard (Xiangrui) Fu</h1>
+                        <p>UI/UX designer & front-end developer @ RISD</p>
+                    </div>
+                    <div className="contactInfo">
+                        <p><a href="https://www.richardfxr.com/" target="_blank">richardfxr.com</a></p>
+                        <p><a href="mailto:xfu01@risd.edu?subject=A%20Witty%20Title" target="_blank">xfu01@risd.edu</a></p>
+                    </div>
+                </header>
 
                 <Section sections={sections} index={1} skipLinkHidden>
                     <dl className="oneCol dlList detailed">
                         <div>
-                            <dt>Rhode Island School of Design (RISD)</dt>
-                            <dd>
-                                <span>2020-2024</span>
-                                <span>Bachelor of Fine Arts, Industrial Design.</span>
-                                <span>3.944 GPA. 5x Honors Designation.</span>
-                            </dd>
-                        </div>
-                        <div>
-                            <dt>Boston University Academy (BUA)</dt>
-                            <dd>
-                                <span>2016-2020</span>
-                                <span>Graduated summa cum laude.</span>
+                            <dt>UX Designer & Front-end Developer @ Fidelity (intern)</dt>
+                            <dd className="ddList">
+                                <p>06/2023 to 08/2023</p>
+                                <ul className="bulletPoints">
+                                    <li>Conducted market research and user interviews to determine an effective project direction.</li>
+                                    <li>Designed and built a production-ready data-driven web app with data visualization.</li>
+                                    <li>Collaborated with other developers and stakeholders to deliver a product that improved employee education.</li>
+                                </ul>
                             </dd>
                         </div>
                     </dl>
@@ -80,35 +91,54 @@ export default function Resume() {
                 <Section sections={sections} index={2} skipLinkHidden>
                     <dl className="oneCol dlList detailed">
                         <div>
+                            <dt>Rhode Island School of Design (RISD)</dt>
+                            <dd>
+                                <span>2020 to 2024</span>
+                                <span>Bachelor of Fine Arts, Industrial Design.</span>
+                                <span>3.956 GPA. 6x Honors Designation.</span>
+                            </dd>
+                        </div>
+                        <div>
+                            <dt>Boston University Academy (BUA)</dt>
+                            <dd>
+                                <span>2016 to 2020</span>
+                                <span>Graduated summa cum laude.</span>
+                            </dd>
+                        </div>
+                    </dl>
+                </Section>
+
+                <Section sections={sections} index={3} skipLinkHidden>
+                    <dl className="oneCol dlList detailed">
+                        <div>
+                            <dt><UnderlinedLink href="https://www.machinists-edge.com/" target="_blank" print="mini-synth.app">Mini Synth</UnderlinedLink></dt>
+                            <dd>
+                                <ul className="bulletPoints">
+                                    <li>Worked with NGOs and refugees to identify digital education shortcomings.</li>
+                                    <li>Collaborated with refugees to design a solution that satisfies their unique needs.</li>
+                                    <li>Developed and deployed a web-based synthesizer app that is used to teach music theory and improve digital literacy.</li>
+                                    <li>Ensured stability and offline functionality through extensive testing.</li>
+                                    <li>Resulted in the creation of 184 songs and 1 teaching session in the first week of deployment.</li>
+                                </ul>
+                            </dd>
+                        </div>
+                        <div>
                             <dt><UnderlinedLink href="https://www.machinists-edge.com/" target="_blank" print="machinists-edge.com">Machinist&#39;s Edge</UnderlinedLink></dt>
                             <dd>
-                                <p>A set of calculators and searchable tables designed for the RISD ID Metal Shop.</p>
-                                <p>Improved CNC milling machine’s operator experience and efficiency; removed operator-related calculation errors.</p>
-                                <p>Features developed based on a primary round of user interviews and continual user testing.</p>
-                                <p>A responsive and accessible front-end web app built with SvelteKit in TypeScript.</p>
+                                <ul className="bulletPoints">
+                                    <li>Conducted user interviews to determine needs regarding CNC machining.</li>
+                                    <li>Designed and Developed a set of calculators and charts to improve operational efficiency and safety.</li>
+                                    <li>Feature updates based on feedback from user testing.</li>
+                                </ul>
                             </dd>
                         </div>
                         <div>
                             <dt><UnderlinedLink href="https://weatherdial.richardfxr.com/" target="_blank" print="weatherdial.richardfxr.com">Weather Dial</UnderlinedLink></dt>
                             <dd>
-                                <p>A weather app focused on intuitive data visualization and animation.</p>
-                                <p>A responsive and accessible web app built with SvelteKit and high-performance native CSS animations.</p>
-                            </dd>
-                        </div>
-                        <div>
-                            <dt><UnderlinedLink to="/info">richardfxr.com</UnderlinedLink></dt>
-                            <dd>
-                                <p>An accessibility-focused redesign of my portfolio site.</p>
-                                <p>Features high contrast mode, low motion, and large touch area in addition to Web Content Accessibility Guidelines (WCAG) 2.0 level AAA compliance.</p>
-                                <p>Website built with React, Next.js, and Framer Motion.</p>
-                            </dd>
-                        </div>
-                        <div>
-                            <dt>Comin</dt>
-                            <dd>
-                                <p>Brand and app redesign that improved brand identity and user experience.</p>
-                                <p>Gamified “post collecting” to drive up user engagement and encourage new posts.</p>
-                                <p>High-fidelity mockups created using Figma, Illustrator, and After Effects.</p>
+                                <ul className="bulletPoints">
+                                    <li>Experimented with data visualization and animations.</li>
+                                    <li>Designed and developed a performance-optimized circular barplot that is accessible to assistive technologies.</li>
+                                </ul>
                             </dd>
                         </div>
                     </dl>
@@ -119,12 +149,13 @@ export default function Resume() {
                     </ul>
                 </Section>
 
-                <Section sections={sections} index={3} skipLinkHidden>
+                <Section sections={sections} index={4} skipLinkHidden>
                     <dl className="oneCol dlList detailed">
                         <div>
                             <dt>Programs</dt>
                             <dd>
                                 <ul>
+                                    <li>Figma</li>
                                     <li>Illustrator</li>
                                     <li>Premiere Pro</li>
                                     <li>After Effects</li>
@@ -133,7 +164,6 @@ export default function Resume() {
                                     <li>Audition</li>
                                     <li>InDesign</li>
                                     <li>DaVinci Resolve</li>
-                                    <li>Figma</li>
                                     <li>SolidWorks</li>
                                     <li>HSMWroks</li>
                                     <li>Blender</li>
@@ -159,8 +189,10 @@ export default function Resume() {
                                     <li>Svelte</li>
                                     <li>Vite</li>
                                     <li>Vue.js</li>
+                                    <li>D3</li>
                                     <li>WebGL</li>
                                     <li>Three.js</li>
+                                    <li>GLSL</li>
                                     <li>Python</li>
                                     <li>Java</li>
                                 </ul>
@@ -173,6 +205,8 @@ export default function Resume() {
                                     <li>UI/UX design</li>
                                     <li>graphic design</li>
                                     <li>product design</li>
+                                    <li>web development</li>
+                                    <li>accessibility</li>
                                     <li>Illustration</li>
                                     <li>photography</li>
                                     <li>videography</li>
